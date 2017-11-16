d0c43d6219e165fd8a7a83ae5a21a355b1883a7d
Olga Vorobyeva <lelik9416@mail.ru>
2017-11-17 00:11:26 +0300

    op

--- task_html.py
+++ task_html.py
@@ -4,56 +4,58 @@ class TagException(Exception):
 
 
 class Tag(object):
-    __slots__ = ('__name', '__attribute')
+    __slots__ = ('__name', '__attribute', '_parent', '_previous_sibling', '_next_sibling', '_first_child', '_last_child', '_children')
     
     def __init__(self, name):
         self.__name = name
         self.__attribute = {}
-    
+        self.__parent = None
+        self.__previous_sibling = None
+        self.__next_sibling = None
     
     @property
     def parent(self):
-        return self.parent
+        return self._parent
     
     
     @parent.setter
     def parent(self, parent):
-        self.parent = parent
+        self._parent = parent
     
     
     @parent.deleter
     def parent(self):
-        del self.parent
+        del self._parent
         
     
     @property
     def previous_sibling(self):
-        return self.previous_sibling
+        return self._previous_sibling
     
     
     @previous_sibling.setter
     def previous_sibling(self, previous_sibling):
-        self.previous_sibling = previous_sibling
+        self._previous_sibling = previous_sibling
     
     
     @previous_sibling.deleter
     def previous_sibling(self):
-        del self.previous_sibling
+        del self._previous_sibling
     
             
     @property
     def next_sibling(self):
-        return self.next_sibling
+        return self._next_sibling
     
     
     @next_sibling.setter
     def next_sibling(self, next_sibling):
-        self.next_sibling = next_sibling
+        self._next_sibling = next_sibling
     
     
     @next_sibling.deleter
     def next_sibling(self):
-        del self.next_sibling
+        del self._next_sibling
     
         
     @property
@@ -102,7 +104,43 @@ class Tag(object):
     
     
 class ContainerTag(Tag):
-    pass
+    __slots__ = ()
+
+    def __init__(self, name, attribute=None):
+        super().__init__(name, attribute)
+        self.__first_child = None
+        self.__last_child = None
+
+    @property
+    def first_child(self, tag):
+        return self._first_child         
+    
+    
+    @property
+    def last_child(self):
+        return self._last_child
+        
+    
+    @property
+    def children(self):
+        a = self._first_child
+        while a:
+            yield a
+            a = a._next_sibling
+        
+
+    def append_child(self, tag):
+        pass
+    
+    
+    def insert_before(self, tag, next_sibling):
+        pass
+    
+    
+
+
+
+
 
 
 
